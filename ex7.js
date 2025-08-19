9df049c38b0460f5a802438d9a98cbd4991af407
Jonathan8 <jonathan.loboda.ifpr.2023@gmail.com>
2025-08-19 14:38:21 -0300

    Delete ex7.js

--- ex7.js
+++
@@ -1,4 +0,0 @@
-valor = parseInt(prompt("Digite o valor da sua compra: "))
-parcela = parseInt(prompt("Digite a quantidade de parcelas "))
-r = valor/parcela
-alert("Parcelado em "+parcela+" vezes ficará com parcelas de: "+r+" reais")
