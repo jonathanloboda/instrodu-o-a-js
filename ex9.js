446ddd87ca41a5767ea8de2a4b8fc99f89f81ab8
Jonathan8 <jonathan.loboda.ifpr.2023@gmail.com>
2025-08-19 14:38:07 -0300

    Delete ex9.js

--- ex9.js
+++
@@ -1,4 +0,0 @@
-valor = parseInt(prompt("Digite o valor do produto: "))
-valorf1 = valor*1.15
-valorf2 = valor*0.85
-alert("O valor do roduto com desconto de 15% é de: "+valorf2+" reais, já o valor do produto com 15% de acréssimo é de: "+valorf1)
