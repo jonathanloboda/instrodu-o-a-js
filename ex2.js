0bd00a69ae85ae34d4c44373c030d5d3ad7a65cb
Jonathan8 <jonathan.loboda.ifpr.2023@gmail.com>
2025-08-19 14:37:22 -0300

    Delete ex2.js

--- ex2.js
+++
@@ -1,5 +0,0 @@
-n1 = parseInt(prompt("Digite um numero"))
-n2 = parseInt(prompt("Digite um numero"))
-n3 = parseInt(prompt("Digite um numero"))
-r = n1*n2*n3
-alert("O resultado da multiplicação dos três numeros é de: "+r)
